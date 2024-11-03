26ce6ef6e4ed71f746af7bad7238243657fc3858
cmatan10 <cmatan10@gmail.com>
2024-11-03 16:45:16 +0200

    first

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  basePath: isProd ? '/solidityQuest' : '',
+  assetPrefix: isProd ? '/solidityQuest/' : '',
+  trailingSlash: true, // Ensures each page is served as a separate directory
+  output: 'export', // Use this for static export compatibility
   experimental: {
-    appDir: false,
+    appDir: false, // Disables the `app` directory if you’re facing issues with it
   },
 };
-
-module.exports = nextConfig;
